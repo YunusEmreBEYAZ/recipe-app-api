689d19cf5e90de02c5bf8f0045c324b34112c881
YunusEmreBEYAZ <yebeyaz@gmail.com>
2024-12-31 11:59:20 +0100

    unit testing implemented

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator app
+"""
+
+def add(x,y):
+    """Add two numbers"""
+    return x + y
+
+def subtract(x,y):
+    """ Subtract two numbers"""
+    return y - x
+
+def multiply(x,y):
+    """ Multiply two numbers"""
+    return x * y
