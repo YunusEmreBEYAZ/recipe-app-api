25f2258dcad2a975335357281050b7f59f33d963
YunusEmreBEYAZ <yebeyaz@gmail.com>
2025-01-07 00:12:58 +0100

    added swagger-ui api documentation GUI

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/swagger-ui/',
-         SpectacularSwaggerView.as_view(url_name='api-schema'), name='swagger-ui'),
+         SpectacularSwaggerView.as_view(url_name='api-schema'),
+         name='swagger-ui'),
 ]
