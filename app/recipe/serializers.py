8f3475e187e401ce6b676e8b567a2e3ab79390cf
YunusEmreBEYAZ <yebeyaz@gmail.com>
2025-01-12 00:44:51 +0100

    recipe api is completed and tested successfully

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -7,11 +7,19 @@ from core.models import Recipe
 
 class RecipeSerializer(serializers.ModelSerializer):
     """ Serializer for recipe object. """
+
+    # with this method we can call user.name (user objects have name attribute)
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'description',
+        fields = ['id', 'user', 'title', 'description',
                   'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+    # here we are returning the name of the user object
+    def get_user(self, obj):
+        return obj.user.name
 
 
 class RecipeDetailSerializer(RecipeSerializer):
